9774a4a313840238fdbfc852ca919f2ef9ac6fbe
Zmijaa <mircevfilip98@gmail.com>
2024-01-09 13:39:50 +0100

    first commit

--- src/Vozilo.java
+++ src/Vozilo.java
@@ -19,22 +19,3 @@ class Vozilo {
 }
 
 
-class Avtomobil extends Vozilo {
-    private int brojNaVrati;
-
-  
-    public Avtomobil(int brzina, int kapacitetNaGorivo, int brojNaVrati) {
-        super(brzina, kapacitetNaGorivo);
-        this.brojNaVrati = brojNaVrati;
-    }
-
-    
-    public void zapali() {
-        System.out.println("Автомобилот е запален.");
-    }
-
-   
-    public int getBrojNaVrati() {
-        return brojNaVrati;
-    }
-}
